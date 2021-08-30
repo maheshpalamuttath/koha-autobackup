ae74194dc8adc1ef12fa027fa136c506a1153466
Mahesh Palamuttath <46676946+maheshpalamuttath@users.noreply.github.com>
2021-08-30 10:49:47 +0530

    Delete backup.sh

--- backup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mysqldump -ukoha_library -ppassword koha_library | gzip > /home/username/gdrive/koha_library-$(date +%d-%m-%Y-%H.%M).sql.gz
-rclone sync /home/username/gdrive/koha_library-$(date +%d-%m-%Y-%H.%M).sql.gz Gdrive:KOHA-DBBACKUPS
-find /home/username/gdrive/* -mtime +3 -exec rm {} \;
